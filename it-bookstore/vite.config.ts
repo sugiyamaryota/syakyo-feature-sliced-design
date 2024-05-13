0edeb7a39ccf002cb2bf3724d8e98ad4caa78647
sugiyamaryota <guqeeky6va8@gmail.com>
2024-05-13 13:25:15 +0900

    add prj

---
+++ it-bookstore/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+export default defineConfig({
+    plugins: [
+        react(),
+        svgr({
+            include: '**/*.svg?react',
+        }),
+    ],
+    base: '/IT-Bookstore/',
+    resolve: {
+        alias: {
+            app: '/src/app',
+            entities: '/src/entities',
+            features: '/src/features',
+            pages: '/src/pages',
+            shared: '/src/shared',
+            widgets: '/src/widgets',
+        },
+    },
+    server: {
+        port: 3000,
+    },
+})
