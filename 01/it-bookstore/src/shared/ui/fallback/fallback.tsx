144f038d9aaf556fd14f69f98f5c79d2d6d83ba8
sugiyamaryota <guqeeky6va8@gmail.com>
2024-05-29 09:48:45 +0900

    add ui/fallback

---
+++ 01/it-bookstore/src/shared/ui/fallback/fallback.tsx
@@ -0,0 +1,23 @@
+import {Link, useRouteError } from 'react-router-dom'
+import ErrorImage from 'shared/assets/images/errorImage.png.svg?react'
+import { RejectedDataType } from 'shared/types'
+
+import './fallback.scss'
+
+export const Fallback = () => {
+    const error = useRouteError()
+    const knownError = error as RejectedDataType
+
+    return (
+        <div role='alert' className='fallback'>
+            <ErrorImage className='fallback__img' />
+            <h1 className='fallback__img'>Something went wrong</h1>
+            <span className='fallback__describe'>
+                {knownError?.messageError} {knownError?.status}
+            </span>
+            <Link to='/' className='fallback__link'>
+                Got to home page
+            </Link>
+        </div>
+    )
+}
