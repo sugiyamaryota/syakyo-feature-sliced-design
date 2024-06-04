2cea410c49556d80ce9536bdd40b1adaa4c87152
sugiyamaryota <guqeeky6va8@gmail.com>
2024-06-04 09:57:32 +0900

    add ui/loaderBookDescription

---
+++ 01/it-bookstore/src/shared/ui/loaders/loaderBookDescription/loaderBookDescription.tsx
@@ -0,0 +1,71 @@
+import { PropsWithChildren } from "react";
+import Skeleton from 'react-loading-skeleton'
+
+import {LoaderBreadcrumbs} from '../loaderBreadcrumbs'
+
+import 'react-loading-skeleton/dist/skeleton.css'
+import './loaderBookDescription.scss'
+
+function StarWrapper({children}: PropsWithChildren<unknown>) {
+    return(
+        <div
+            style={{
+                display: 'inline-block',
+                clipPath:
+                    'polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)',
+                width: '22px',
+                height: '22px'
+            }}
+        >
+            {children}
+        </div>
+    )
+}
+
+export const LoaderBookDescription = () => {
+    return (
+        <div className='loader-book-description _container'>
+            <LoaderBreadcrumbs />
+            <div className='loader-book-description__content'>
+                <Skeleton className='loader-book-description__img' />
+                <div className='loader-book-description__info'>
+                    <Skeleton
+                        count={5}
+                        wrapper={StarWrapper}
+                        height='100%'
+                        duration={0.9}
+                    />
+                    <h1>
+                        <Skeleton />
+                    </h1>
+                    <h2>
+                        <Skeleton />
+                    </h2>
+                    <h3>
+                        <Skeleton />
+                    </h3>
+                    <div className='loader-book-description__info-list'>
+                        <Skeleton count={5} height={14} />
+                    </div>
+                    <div className='loader-book-description__price'>
+                        Price: <Skeleton count={60} height={40} />
+                    </div>
+                    <div className='loader-book-description__wrapper-button'>
+                        <Skeleton className='loader-book-description__button' />
+                    </div>
+                </div>
+                <div className='loader-book-description__wrapper-authors'>
+                    <Skeleton className='loader-book-description__authors' />
+                </div>
+            </div>
+            <div className='loader-book-description__desc'>
+                <h1 className='loader-book-description__desc-title'>
+                    <Skeleton />
+                </h1>
+                <p>
+                    <Skeleton count={6} />
+                </p>
+            </div>
+        </div>
+    )
+}
