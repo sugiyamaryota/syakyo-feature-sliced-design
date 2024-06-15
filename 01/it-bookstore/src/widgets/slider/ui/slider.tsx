615fe50c7fa607973150a0b7a5c76d97bffed826
sugiyamaryota <guqeeky6va8@gmail.com>
2024-06-15 15:45:29 +0900

    add Slider

---
+++ 01/it-bookstore/src/widgets/slider/ui/slider.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+import clsx from 'clsx'
+import { ISlides } from 'shared/types'
+import { Carousel } from 'shared/ui/carousel'
+
+import './slider.scss'
+
+interface ISlider {
+    readonly slides: ISlides[]
+    readonly className?: string
+}
+
+export const Slider: FC<ISlider> = (props) => {
+    const {slides, className} = props
+    const slideImages = slides.map((item) => (
+        <Link key={item.img} to={item.url}>
+            <img src={item.img} alt={item.alt} className="slider__item" />
+        </Link>
+    ))
+    return (
+        <div className='slider__wrapper'>
+            <Carousel className={clsx('slider', className)}>
+                {slideImages}
+            </Carousel>
+        </div>
+    )
+}
